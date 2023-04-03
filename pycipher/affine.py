3c9d5350ad5bd8233a8747d265b6f7820562b30b
Alllannnn <114339874+Alllannnn@users.noreply.github.com>
2023-04-03 12:11:00 +0300

    Update affine.py

--- pycipher/affine.py
+++ pycipher/affine.py
@@ -1,8 +1,4 @@
-'''
-implements affine substitution cipher
-Author: James Lyons
-Created: 2012-04-28
-'''
+
 from pycipher.base import Cipher
 
 ####################################################################################
@@ -15,7 +11,6 @@ class Affine(Cipher):
     where c is the ciphertext letter, p the plaintext letter.
     *b* is an integer 0-25, *a* is an integer that has an inverse (mod 26).
     Allowable values for *a* are: 1,3,5,7,9,11,15,17,19,21,23,25 
-    For more info on the Affine cipher see http://www.practicalcryptography.com/ciphers/affine-cipher/
     
     :param a: The multiplicative part of the key. Allowable values are: 1,3,5,7,9,11,15,17,19,21,23,25 
     :param b: The additive part of the key. Allowable values are integers 0-25
